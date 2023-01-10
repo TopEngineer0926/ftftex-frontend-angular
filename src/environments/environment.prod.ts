667a341e2fa43f2415d84f742e031e145080988c
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-01-10 19:02:01 +0400

    add env variables and staging env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: "https://api.ftftx.com/",
+  baseUrl2: "https://verification.ftftx.com/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: "https://stgapi.ftftx.com/",
+  baseUrl2: "https://staging-verification.ftftx.com/"
 };
 
 /*
