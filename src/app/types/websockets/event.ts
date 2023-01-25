8b581f3bca3dfa13b50e69ce623baa8f8a88076b
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-01-25 19:05:15 +0400

    create class for requesting okx

---
+++ src/app/types/websockets/event.ts
@@ -0,0 +1,25 @@
+import { WsChannel } from './request';
+
+export interface WsEvent {
+  event: 'error' | 'login' | 'subscribe' | 'unsubscribe';
+  code?: string;
+  msg?: string;
+  arg?: any;
+  data?: any;
+}
+
+export interface WsDataEvent {
+  arg: {
+    channel: WsChannel;
+    uid?: string;
+    instId?: string;
+    instFamily?: string;
+  };
+  data: any;
+}
+
+export interface WsLoginEvent extends WsEvent {
+  event: 'login';
+}
+
+export type WsResponse = WsEvent;
