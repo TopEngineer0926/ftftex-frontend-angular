2ab8950ed6fee983b55128a3a209c934522b081e
arthur <gyulabyana@gmail.com>
2023-02-05 20:53:25 +0400

    add transfer tab

--- src/app/wallet/transfer/transfer.component.ts
+++ src/app/wallet/transfer/transfer.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {RestClient} from "../../rest-client";
+
+const client = new RestClient({
+  apiKey: '7f9eedd1-56d7-4555-9d2b-2242668d4f2e',
+  // apiKey: 'apiKeyHere',
+  apiSecret: '5F28B22584E8689C5FF1E67C6BEC75DA',
+  // apiSecret: 'apiSecretHere',
+  apiPass: 'FTFTxOKX@123)',
+  // apiPass: 'apiPassHere',
+});
 
 @Component({
   selector: 'app-transfer',
   templateUrl: './transfer.component.html',
   styleUrls: ['./transfer.component.scss']
 })
-export class TransferComponent implements OnInit {
 
+export class TransferComponent implements OnInit {
   selectedCoin: string;
+  amount: string;
+  fromMain: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
@@ -15,6 +27,19 @@ export class TransferComponent implements OnInit {
 
   selectCoin() {
     this.selectedCoin = 'BTC'
+  }
+
+  transfer() {
+
+    const params = {
+      ccy: this.selectedCoin,
+      amt: this.amount,
+      from: '6',
+      to: '18'
+    }
+    client.fundsTransfer(params).then((res) => {
+      console.log(res, 'res');
+    })
 
   }
 
