fee8e6ee1a4da31cc414fd30b4a2d07e4526e30d
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-02-17 00:51:15 +0400

    add new slieds

--- src/app/home/main-carousal/main-carousal.component.ts
+++ src/app/home/main-carousal/main-carousal.component.ts
@@ -12,21 +12,21 @@ export class MainCarousalComponent implements OnInit {
 
   LoggedIn = {0: ''};
   Slides = [
-    {type: 'image' , image: 'assets/images/slides/1.svg' , link: ''},
+    {type: 'image' , image: 'assets/images/slides/f2.png' , link: ''},
     {type: 'video' , image: 'assets/images/slides/v1.png' , link: 'https://www.youtube.com/watch?v=EJzB_Fa27ko'},
     {type: 'video' , image: 'assets/images/slides/v2.png' , link: 'https://www.youtube.com/watch?v=FExp9YuTzbY'},
-    {type: 'image' , image: 'assets/images/slides/2.svg' , link: ''},
+    {type: 'image' , image: 'assets/images/slides/f4.png' , link: ''},
     {type: 'video' , image: 'assets/images/slides/v3.png' , link: 'https://www.youtube.com/watch?v=KHm0uUPqmVE'},
-    {type: 'image' , image: 'assets/images/slides/3.svg' , link: 'https://blog.ftftx.com/'},
-    {type: 'image' , image: 'assets/images/slides/4.svg' , link: ''},
+    {type: 'image' , image: 'assets/images/slides/f1.png' , link: ''},
+    {type: 'image' , image: 'assets/images/slides/f3.png' , link: ''},
   ];
 
   SlidesMB = [
-    {type: 'image' , image: 'assets/images/slides/1.svg' , link: ''},
+    {type: 'image' , image: 'assets/images/slides/f2.png' , link: ''},
     {type: 'video' , image: 'assets/images/slides/v2.png' , link: 'https://www.youtube.com/watch?v=FExp9YuTzbY'},
-    {type: 'image' , image: 'assets/images/slides/2.svg' , link: ''},
-    {type: 'image' , image: 'assets/images/slides/3.svg' , link: 'https://blog.ftftx.com/'},
-    {type: 'image' , image: 'assets/images/slides/4.svg' , link: ''},
+    {type: 'image' , image: 'assets/images/slides/f4.png' , link: ''},
+    {type: 'image' , image: 'assets/images/slides/f1.png' , link: ''},
+    {type: 'image' , image: 'assets/images/slides/f3.png' , link: ''},
   ];
 
   isMobile = false;
